6fdfbe5577dc08d528a194c1c5807728c11074a3
TEldridge <skippydrumskins@yahoo.com>
2020-07-16 16:37:26 -0500

    five passing, fixing detect win

--- main.js
+++ main.js
@@ -22,6 +22,16 @@ let board = [
 // assigns the first mark as 'X'
 // using let because the variable is expected to change from 'X' to 'O' and back
 let playerTurn = 'X';
+let turn = 0;
+
+
+const changeMarker = () => {
+  if(playerTurn === "X"){
+    playerTurn = "O"
+  } else {
+    playerTurn = "X"
+  }
+}
 
 // is a function that print the current status of the board using the variable - board
 const printBoard = () => {
@@ -34,25 +44,103 @@ const printBoard = () => {
 }
 
 const horizontalWin = () => {
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")){
+    return true
+  } else if ((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+  || (board[1][0] == "O" && board[1][1] == "O" && boar0d[1][2] == "O")){
+    return true
+  } else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")){
+    return true
+  } else {
+    return false
+  }
+
+  // it('should check for horizontal wins', () => {
+  //   board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
+  //   assert.equal(horizontalWin(), true);
+  // });
   // Your code here to check for horizontal wins
 }
 
 const verticalWin = () => {
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+  || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")){
+    return true
+  }else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+  || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")){
+    return true
+  } else if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+  || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")){
+  return true
+} else {
+  return false
+}
+
+  // it('should check for vertical wins', () => {
+  //   board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
+  //   assert.equal(verticalWin(), true);
+  // });
   // Your code here to check for vertical wins
 }
 
 const diagonalWin = () => {
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")){
+    return true
+  }else if((board[0][2] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+  || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")){
+    return true
+  } else {
+    return false
+  }
+  // it('should check for diagonal wins', () => {
+  //   board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
+  //   assert.equal(diagonalWin(), true);
+  // });
   // Your code here to check for diagonal wins
 }
 
-const checkForWin = () => {
-  // Your code here call each of the check for types of wins
-}
 
 const ticTacToe = (row, column) => {
   // Your code here to place a marker on the board
-  // then check for a win
+  board[row][column] = playerTurn
+  changeMarker()
+  turn += 1;
+  if (turn >= 5){
+    checkForWin()
+  }else {
+    turn +=1
+  }
+  
+     
 }
+  // it('should place mark on the board', () => {
+  //   ticTacToe(1, 1);
+  //   assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+  // });
+  // it('should alternate between players', () => {
+  //   ticTacToe(0, 0);
+  //   assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+  // });
+
+ 
+  
+  
+
+  // then check for a win
+
+
+
+
+const checkForWin = () => {
+  // it('should detect a win', () => {
+  //   assert.equal(checkForWin(), true);
+  // });
+  // Your code here call each of the check for types of wins
+}
+
 
 const getPrompt = () => {
   printBoard();
