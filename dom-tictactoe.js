fedbe80f2ca3996960f89cad115da812bf376705
Kd125 <kashdangar1250@gmail.com>
2023-01-18 20:03:57 -0600

    New updates

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -53,10 +53,8 @@ const updateBoard = (id) => {
 const checkForWin = () => {
   // calls each checkForWin possibility and if any are true gives a page alert,
   if(horizontalWin() || verticalWin() || diagonalWin()) {
-    // **BONUS** you could make the dismissal of this alert window reset the board...
     window.alert(`Player ${currentMarker} won!`)
   } else {
-    // if no win, change the marker from X to O, or O to X for the next player.
     changeMarker()
   }
 }
